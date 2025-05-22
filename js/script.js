31a4d7d6036b765a4e3e597cbbe8f51c1a7474d0
hitozz0 <wisnu.putra8506@gmail.com>
2025-05-22 13:56:44 +0800

    add-on

--- js/script.js
+++ js/script.js
@@ -7,3 +7,29 @@ function troll() {
  function troll3() { 
     alert("oke deh");
  };
+
+ function hitung(value) {
+  a=  document.getElementsById('n1').value;
+  b= document.getElementsById('n2').value;
+  h= a+b;
+  alert("hasilnya adalah "+h);
+ }
+
+
+
+
+ function angka(value){
+   let txt;
+   let a = document.getElementById('angka').value;
+   if(a%2==0){
+      txt="genap";
+   }else{
+      txt="ganjil";
+   }
+   alert(txt);
+ }
+
+ function func(){
+   const nama = prompt("masukan nama anda");
+   alert(nama);
+ }
